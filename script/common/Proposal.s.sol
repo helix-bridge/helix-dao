4501719b8f402c2282486e0cc47f64923e435fa4
xiaoch05 <xiaoch2010@gmail.com>
2024-07-12 11:31:48 +0800

    Move Protocal Fee Receiver (#14)
    * Move Protocal Fee Receiver
    
    * rmv linea
    
    * split
    
    * executed
    
    ---------
    
    Co-authored-by: echo <hujw77@gmail.com>

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {LnBridgeV3OwnerChecker11} from "../11_LnBridgeV3OwnerChecker.s.sol";
+import {MoveProtocolFeeReceiver10} from "../10_MoveProtocolFeeReceiver.s.sol";
 
-contract Proposal is LnBridgeV3OwnerChecker11 {}
+contract Proposal is MoveProtocolFeeReceiver10 {}
