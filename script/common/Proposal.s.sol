9649d5c77485cf17b5c713ba6ffeef24d2b6e5a5
xiaoch05 <xiaoch2010@gmail.com>
2024-07-24 15:48:13 +0800

    connect & register avalanche

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {RegisterUsdcToken12} from "../12_RegisterUsdcToken.s.sol";
+import {AddAvalancheMantle13} from "../13_AddAvalancheMantle.s.sol";
 
-contract Proposal is RegisterUsdcToken12 {}
+contract Proposal is AddAvalancheMantle13 {}
