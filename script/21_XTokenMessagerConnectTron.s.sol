2bd833ab6b0536ec3661968c9f2af83c4850a770
echo <hujw77@gmail.com>
2024-08-28 12:22:09 +0800

    set white list

--- script/21_XTokenMessagerConnectTron.s.sol
+++ script/21_XTokenMessagerConnectTron.s.sol
@@ -7,12 +7,16 @@ import {safeconsole} from "forge-std/safeconsole.sol";
 interface IMsgportMessager {
     function setRemoteMessager(uint256 _appRemoteChainId, uint256 _msgportRemoteChainId, address _remoteMessager)
         external;
+
+    function setWhiteList(address _caller, bool _enable) external;
 }
 
 contract XTokenMessagerConnectTron21 is Base {
     IMsgportMessager messager = IMsgportMessager(0x65Be094765731F394bc6d9DF53bDF3376F1Fc8B0);
+	address backing = 0x1cA4975a056cF6885789A4FfB13B147da07e9Ebb;
 
     function run() public sphinx {
         messager.setRemoteMessager(728126428, 728126428, 0x13Fd60a93feD8141875378Ba57500c5E554C93F2);
+		messager.setWhiteList(backing, true);
     }
 }
