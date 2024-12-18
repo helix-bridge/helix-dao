3b35be4583df650cbafe48365cd2dd5ab81e491e
echo <hujw77@gmail.com>
2024-12-18 15:14:52 +0800

    accept xtoken ownership on crab

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AddMorphArbitrum23} from "../23_AddMorphArbitrum.s.sol";
+import {AcceptOwnership24} from "../24_AcceptOwnership.s.sol";
 
-contract Proposal is AddMorphArbitrum23 {}
+contract Proposal is AcceptOwnership24 {}
