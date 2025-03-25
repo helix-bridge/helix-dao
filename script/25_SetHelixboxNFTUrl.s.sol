d5f182a3a7ae5355f06fea0b7ad74aaf19426395
echo <hujw77@gmail.com>
2025-03-25 14:08:37 +0800

    uri

--- script/25_SetHelixboxNFTUrl.s.sol
+++ script/25_SetHelixboxNFTUrl.s.sol
@@ -5,13 +5,18 @@ import {Base} from "./common/Base.sol";
 
 interface III {
     function setURI(string memory) external;
+    function setBaseURI(string memory baseURI) public;
+    function setURI(uint256 tokenId, string memory tokenURI) public;
 }
 
 contract SetHelixboxNFTUrl is Base {
-    address nft = 0xB0262b25cc745A0AA6bd2ac914BF4b5F77506369;
+    address nft = 0x418B17066233977aD0CFf6FB9d6029b70804C29E;
 
     function run() public sphinx {
         address dao = safeAddress();
         III(nft).setURI("ipfs://bafybeiflaokhjwqnimgoz55a274ufeyvzjiyxvnk4iucwxwmbptaaab24a/{id}.json");
+        III(nft).setBaseURI("ipfs://bafybeiflaokhjwqnimgoz55a274ufeyvzjiyxvnk4iucwxwmbptaaab24a/");
+        III(nft).setURI(1, "1.json");
+        III(nft).setURI(2, "2.json");
     }
 }
