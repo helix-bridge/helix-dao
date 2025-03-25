a219892daa311e008df2cee4825f23dd3776a439
echo <hujw77@gmail.com>
2025-03-25 11:44:59 +0800

    fix

--- script/25_SetHelixboxNFTUrl.s.sol
+++ script/25_SetHelixboxNFTUrl.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {Base} from "./common/Base.sol";
 
 interface III {
-    function setURI(string) external;
+    function setURI(string memory) external;
 }
 
 contract SetHelixboxNFTUrl is Base {
