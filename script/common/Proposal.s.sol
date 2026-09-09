5ee80717a071e84758a798330c55a673325a7eed
echo <hujw77@gmail.com>
2026-09-09 20:55:59 +0800

    sunset zircuit

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {SunsetArbitrumPolygon} from "../30_SunsetArbitrumPolygon.s.sol";
+import {UpgradeLnBridge27} from "../27_UpgradeLnBridgeV3.s.sol";
 
-contract Proposal is SunsetArbitrumPolygon {}
+contract Proposal is UpgradeLnBridge27 {}
